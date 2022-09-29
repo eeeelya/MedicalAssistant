9dcb3e42a71365aab541392e1a26615fe894697b
Goroshchik Ilya <goooroshchik@gmail.com>
2022-09-29 20:10:27 +0300

    Fix models

--- medical_assistant/core/abstract_models.py
+++ medical_assistant/core/abstract_models.py
@@ -1,7 +1,6 @@
 import datetime
 
 from django.db import models
-from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
 
 
--- medical_assistant/doctor/models.py
+++ medical_assistant/doctor/models.py
@@ -6,16 +6,16 @@ from core.abstract_models import SpecialInformation, UserInformation
 
 class Doctor(UserInformation, SpecialInformation):
     class Department(models.TextChoices):
-        THERAPY = "T", _("Therapy")
-        DERMATOLOGY = "D", _("Dermatology")
-        GYNECOLOGY = "G", _("Gynecology")
-        SURGERY = "S", _("Surgery")
+        THERAPY = "THER", _("Therapy")
+        DERMATOLOGY = "DERM", _("Dermatology")
+        GYNECOLOGY = "GYNE", _("Gynecology")
+        SURGERY = "SURG", _("Surgery")
         CHILDREN_THERAPY = "CT", _("Children's therapy")
-        NEUROLOGY = "N", _("Neurology")
-        PSYCHOTHERAPY = "P", _("Psychotherapy")
-        CARDIOLOGY = "C", _("Cardiology")
-        ONCOLOGY = "O", _("Oncology")
-        DENTISTRY = "D", _("Dentistry")
+        NEUROLOGY = "NEUR", _("Neurology")
+        PSYCHOTHERAPY = "PSYC", _("Psychotherapy")
+        CARDIOLOGY = "CARD", _("Cardiology")
+        ONCOLOGY = "ONCO", _("Oncology")
+        DENTISTRY = "DENT", _("Dentistry")
 
     class Category(models.TextChoices):
         HIGH = "H", _("High")
@@ -23,7 +23,7 @@ class Doctor(UserInformation, SpecialInformation):
         SECOND = "2", _("Second")
 
     category = models.CharField(max_length=1, choices=Category.choices, default=Category.SECOND)
-    department = models.CharField(max_length=2, choices=Department.choices, default=Department.THERAPY)
+    department = models.CharField(max_length=4, choices=Department.choices, default=Department.THERAPY)
 
     class Meta:
         db_table = "doctor"
