6f5bd128f63ad58bb456dd4f9f3949e822d8d8cd
Goroshchik Ilya <goooroshchik@gmail.com>
2023-02-05 12:55:38 +0300

    This functionality is moved to a separate microservice

--- medical_assistant/calculations/choices.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.utils.translation import gettext_lazy as _
-
-
-class ModelType(models.TextChoices):
-    SEGMENTATION = "SEGM", _("Segmentation")
-    # TODO: add more models
--- medical_assistant/core/storage_backends.py
+++
@@ -1,6 +0,0 @@
-from storages.backends.s3boto3 import S3Boto3Storage
-
-
-class ImageStorage(S3Boto3Storage):
-    location = ""
-    file_overwrite = False
